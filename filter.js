ad760ec1dd1ea606eb049e83dd001b8f28fc304c
Nathan Samano <nsv9982@saintjoe.edu>
2015-02-14 01:15:15 -0500

    filtering for a specifc field (looking for fire type)

---
+++ filter.js
@@ -0,0 +1,27 @@
+// filter code for json project
+
+var data = require('./types.js');
+var types = JSON.parse(data);
+
+function filter(arr, type) {
+  var passed = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (type(arr[i]))
+      passed.push(type[i]);
+  }
+  return passed;
+}
+
+// get the whole entry where type is Fire (I know this is wrong)
+var getFire = types.filter(function(field) {
+  if (field == "Fire")
+    return types.field;
+});
+
+// print out one entry
+/*console.log(JSON.stringify(filter(types, function(key) {
+  return key.type == "Fire";
+})));*/
+
+console.log(types);    // prints everything
+console.log(getFire);  // supposed to print the line where type == "Fire"
